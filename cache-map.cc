db8811b668f050ddc6597de5335a064317a24374
Jeff Muizelaar <jmuizelaar@mozilla.com>
2016-06-21 18:43:13 -0400

    Adjust whitespace

--- cache-map.cc
+++ cache-map.cc
@@ -14,36 +14,37 @@
 static const uint32_t kGoldenRatioU32 = 0x9E3779B9U;
 int hash(int a)
 {
-        return kGoldenRatioU32 * a;
+    return kGoldenRatioU32 * a;
 }
 typedef int Index;
 template <class Key, class Value>
 class CacheMap {
-        public:
-        CacheMap()
-        {
-                for (int i = 0; i < htable_size; i++) {
-                        htable[i] = empty;
-                }
-                for (int i = 0; i < SIZE; i++) {
-                        table[i].mru_next = i + 1;
-                        table[i].mru_prev = i - 1;
-                        table[i].next = i;
-                        table[i].prev = i;
-                }
-                table[0].mru_prev = SIZE-1;
-                table[SIZE-1].mru_next = 0;
-                mru = 0;
+public:
+    CacheMap()
+    {
+        for (int i = 0; i < htable_size; i++) {
+            htable[i] = empty;
         }
-                template <class T>
-        const Value get(Key key, T compute) {
+        for (int i = 0; i < SIZE; i++) {
+            table[i].mru_next = i + 1;
+            table[i].mru_prev = i - 1;
+            table[i].next = i;
+            table[i].prev = i;
+        }
+        table[0].mru_prev = SIZE-1;
+        table[SIZE-1].mru_next = 0;
+        mru = 0;
+    }
+
+    template <class T>
+    const Value get(Key key, T compute) {
         const int mask = (1 <<  htable_size_bits) - 1;
 
 #if 0
         for (int i = 0; i < htable_size; i++) {
-                if (htable[i] != empty) {
-                        assert((hash(table[htable[i]].key) & mask) == i);
-                }
+            if (htable[i] != empty) {
+                assert((hash(table[htable[i]].key) & mask) == i);
+            }
         }
 #endif
 
@@ -52,31 +53,31 @@ class CacheMap {
 
         Index orig = n;
         if (n != empty) do {
-                if (table[n].key == key) {
-                        if (mru == n)
-                                return table[n].value;
-                        // remove from old location
-                        Index next = table[n].mru_next;
-                        Index prev = table[n].mru_prev;
-                        table[prev].mru_next = next;
-                        table[next].mru_prev = prev;
+            if (table[n].key == key) {
+                if (mru == n)
+                    return table[n].value;
+                // remove from old location
+                Index next = table[n].mru_next;
+                Index prev = table[n].mru_prev;
+                table[prev].mru_next = next;
+                table[next].mru_prev = prev;
 
-                        // insert at head
-                        prev = table[mru].mru_prev;
-                        table[n].mru_prev = prev;
-                        table[n].mru_next = mru;
-                        table[mru].mru_prev = n;
-                        table[prev].mru_next = n;
+                // insert at head
+                prev = table[mru].mru_prev;
+                table[n].mru_prev = prev;
+                table[n].mru_next = mru;
+                table[mru].mru_prev = n;
+                table[prev].mru_next = n;
 
-                        mru = n;
-                        // rotate the linked list so that n is at the head
-                        // alternatively we could move the entry as part of the
-                        // list so that each cache bucket is also mru ordered
-                        htable[h & mask] = n;
-                        return table[n].value;
-                } else {
-                        n = table[n].next;
-                }
+                mru = n;
+                // rotate the linked list so that n is at the head
+                // alternatively we could move the entry as part of the
+                // list so that each cache bucket is also mru ordered
+                htable[h & mask] = n;
+                return table[n].value;
+            } else {
+                n = table[n].next;
+            }
         } while (n != orig);
 
         Index lru = table[mru].mru_prev;
@@ -84,11 +85,11 @@ class CacheMap {
         int lru_h = hash(table[lru].key);
         // XXX we need to update htable if it currently points at us
         if (htable[lru_h & mask] == lru) {
-                if (table[lru].next == lru) {
-                        htable[lru_h & mask] = empty;
-                } else {
-                        htable[lru_h & mask] = table[lru].next;
-                }
+            if (table[lru].next == lru) {
+                htable[lru_h & mask] = empty;
+            } else {
+                htable[lru_h & mask] = table[lru].next;
+            }
         }
         // remove from old location
         Index next = table[lru].next;
@@ -98,14 +99,14 @@ class CacheMap {
 
         // insert at head
         if (orig == empty) {
-                table[lru].next = lru;
-                table[lru].prev = lru;
+            table[lru].next = lru;
+            table[lru].prev = lru;
         } else {
-                prev = table[orig].prev;
-                table[lru].prev = prev;
-                table[lru].next = orig;
-                table[orig].prev = lru;
-                table[prev].next = lru;
+            prev = table[orig].prev;
+            table[lru].prev = prev;
+            table[lru].next = orig;
+            table[orig].prev = lru;
+            table[prev].next = lru;
         }
         table[lru].key = key;
         table[lru].value = compute(key);
@@ -113,20 +114,27 @@ class CacheMap {
         mru = lru;
         htable[h & mask] = lru;
         return table[lru].value;
-}
+    }
 private:
-        struct { Key key; Value value; Index next; Index prev; Index mru_next; Index mru_prev; } table[SIZE];
-        Index mru;
-        Index htable[htable_size];
+    struct {
+        Key key;
+        Value value;
+        Index next;
+        Index prev;
+        Index mru_next;
+        Index mru_prev;
+    } table[SIZE];
+    Index mru;
+    Index htable[htable_size];
 };
 
 int main()
 {
-        CacheMap<int, int> m;
-        for (int i=0; i<260; i++) {
-                printf("%d\n", m.get(i, [](int x) { printf("get %d'\n", x); return x + 1; }));
-        }
-        for (int i=259; i>=0; i--) {
-                printf("%d\n", m.get(i, [](int x) { printf("get %d'\n", x); return x + 1; }));
-        }
+    CacheMap<int, int> m;
+    for (int i=0; i<260; i++) {
+        printf("%d\n", m.get(i, [](int x) { printf("get %d'\n", x); return x + 1; }));
+    }
+    for (int i=259; i>=0; i--) {
+        printf("%d\n", m.get(i, [](int x) { printf("get %d'\n", x); return x + 1; }));
+    }
 }
